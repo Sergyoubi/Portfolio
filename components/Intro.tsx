2e2a2ee19a50eac1c363aef963d280321fa6c1c2
Sergyoubi <sergyoubi@gmail.com>
2024-07-29 17:04:53 +0300

    removed hover effect

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => {
     <section className="w-full h-full bg-slate-900 flex justify-center lg:justify-end items-center">
       <div className="w-[80%] h-[70%] lg:w-[50%] lg:h-[68%] flex flex-col justify-between lg:justify-start items-center gap-5">
         <div className="w-full h-[80%] lg:w-[80%] lg:h-[50%] flex flex-col gap-7">
-          <p className="text-lg font-light hover:font-semibold text-slate-400 tracking-wide leading-relaxed">
+          <p className="text-lg font-light text-slate-400 tracking-wide leading-relaxed">
             My expertise lies in crafting robust and dynamic web applications
             that not only meet functional requirements but also delight users
             with their aesthetics and usability.
