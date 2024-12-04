1de9f94115d6b490b588208946849fc43b87b368
Sergyoubi <sergyoubi@gmail.com>
2024-12-04 14:42:02 +0300

    adjusted dimension

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -28,9 +28,9 @@ const TechStack = () => {
           <p className="text-slate-300 text-base font-medium">My tech stack:</p>
         </div>
       </section>
-      <section className="w-full h-[50%] xl:h-[75%] 2xl:h-[70%] bg-slate-900 flex justify-center items-start lg:justify-end lg:items-center">
-        <div className="w-[85%] h-[80%] xl:w-[50%] xl:h-[99%] flex justify-center items-start">
-          <div className="w-[80%] h-[95%] glassmorphisme-class flex-center">
+      <section className="w-full h-[60%] xl:h-[75%] 2xl:h-[70%] bg-slate-900 flex justify-center items-start lg:justify-end lg:items-center">
+        <div className="w-[85%] h-[70%] xl:w-[50%] xl:h-full flex justify-center items-start">
+          <div className="w-[95%] xl:w-[82%] xl:h-full glassmorphisme-class flex-center">
             <IconCloud iconSlugs={slugs} />
           </div>
         </div>
